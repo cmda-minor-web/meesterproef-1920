e864b02773386530494bd53909ed0d1c45ea592b
Koop Reynders <mail@kopo.nl>
2020-06-17 23:47:10 +0200

    programma en projecten, expo v0,8

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,12 @@
+
+
+/*
+  *** HASH ***
+  In het menu staan 3 knoppen,
+  die verwijzen met een hash naar bijhorende artikelen.
+  Het tweede menu krijgt een highlight als er op is geklikt.
+*/
+
 const knoppen = document.querySelectorAll("main nav a");
 // const nav = document.querySelector("main nav");
 
@@ -16,4 +25,33 @@ function clear_selected(){
     knop.classList.remove("selected");
   })
 }
-set_selected(); //init
+if(location.hash) set_selected(); //init
+
+
+/*
+  *** INTERSECTION OBSERVER ***
+  Als er wordt gescrollt,
+  krijgt het menu een highlight van het betreffende onderdeel
+
+  MDN https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+  https://alligator.io/js/intersection-observer
+*/
+
+// const programma = document.querySelector('#programma');
+// const projecten = document.querySelector('#projecten');
+// const about = document.querySelector('#about');
+
+
+
+observer = new IntersectionObserver(entries => {
+  console.log(entries[0].target.id);
+  //location.hash = entries[0].target.id;
+});
+
+// const articles = document.querySelectorAll('main > article');
+// articles.forEach(article => {
+//   observer.observe(article);
+// });
+
+const projecten = document.querySelector('#about');
+observer.observe(projecten);
